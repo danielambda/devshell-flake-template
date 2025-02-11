c1808f865cf198b11f301911bc1f41cff7613bef
danielambda <daniel.gevorgyan25@gmail.com>
2025-02-11 21:48:57 +0300

    chore: flakified

--- flake.nix
+++ flake.nix
@@ -1,7 +1,24 @@
 {
   description = "devShell flake template";
-
-  outputs = _: {
-    defaultTemplate.path = ./template;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        defaultTemplate.path = ./template;
+
+        devShells.default = pkgs.mkShell {
+          packages = [pkgs.nixd];
+        };
+      }
+    );
 }
